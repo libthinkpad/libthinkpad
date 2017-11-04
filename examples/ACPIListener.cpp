609b9b1e1ed871340ee01b92231c1113e7085ac7
Ognjen Galic <smclt30p@gmail.com>
2017-11-04 15:28:19 +0100

    docs: add acpi example

---
+++ examples/ACPIListener.cpp
@@ -0,0 +1,48 @@
+#include "libthinkpad.h"
+
+#include <iostream>
+
+using ThinkPad::PowerManagement::ACPI;
+using ThinkPad::PowerManagement::ACPIEvent;
+using ThinkPad::PowerManagement::ACPIEventHandler;
+
+class ACPIHandler : public ACPIEventHandler {
+
+public:
+
+    void handleEvent(ACPIEvent event) {
+
+        switch (event) {
+
+            case ACPIEvent::DOCKED:
+                std::cout << "ThinkPad was docked" << std::endl;
+                break;
+            case ACPIEvent::UNDOCKED:
+                std::cout << "ThinkPad was undocked" << std::endl;
+                break;
+            case ACPIEvent::LID_CLOSED:
+                std::cout << "ThinkPad lid was closed" << std::endl;
+                break;
+            case ACPIEvent::LID_OPENED:
+                std::cout << "ThinkPad lid was opened" << std::endl;
+                break;
+
+        }
+
+    }
+
+};
+
+int main(void) {
+
+
+    ACPI *acpi = new ACPI();
+    ACPIHandler *handler = new ACPIHandler();
+
+    acpi->setEventHandler(handler);
+    acpi->wait();
+
+    delete acpi;
+    delete handler;
+
+}
